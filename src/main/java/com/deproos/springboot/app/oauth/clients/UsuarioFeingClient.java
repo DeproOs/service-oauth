791f62489a4927d13296d9a60ca16ce7d4639013
DeproOs <klaudio_4ever@hotmail.com>
2023-11-27 13:49:30 -0300

    primer commit

---
+++ src/main/java/com/deproos/springboot/app/oauth/clients/UsuarioFeingClient.java
@@ -0,0 +1,13 @@
+package com.deproos.springboot.app.oauth.clients;
+
+import com.deproos.springboot.usuarios.commons.models.entity.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeingClient {
+
+    @GetMapping("/usuarios/search/buscar-username")
+    public Usuario findByUsername(@RequestParam String username);
+}
